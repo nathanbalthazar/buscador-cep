c39bbf3c9966e4e8a1997db208b59decb0359743
Nathan Balthazar <nathanbalthazar96@gmail.com>
2022-04-06 16:12:54 -0300

    feat: create Container and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-function App() {
+import { FiSearch } from 'react-icons/fi'
+
+import { Container } from './styles/styles'
+
+
+export function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Container>
+      <h1 className='title'>Buscador CEP</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu CEP"/>
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+        <main className='main'>
+          <h2>CEP: 2298792</h2>
+
+          <span>Rua Teste da Silva</span>
+          <span>Complemento: teste da silva</span>
+          <span>Vila Teste</span>
+          <span>Teste - TS</span>
+        </main>
+
+    </Container>
   )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { App } from './App'
 
-import { GlobalStyles } from './styles/global'
+import { GlobalStyles } from './styles/styles'
 
 ReactDOM.render(
   <React.StrictMode>

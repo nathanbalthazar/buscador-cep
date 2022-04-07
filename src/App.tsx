095fd98bf2be3b7257e7cdc9336dbce65ac0e2c5
Nathan Balthazar <nathanbalthazar96@gmail.com>
2022-04-07 17:05:23 -0300

    feature: add App types and api

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,66 @@
-import { FiSearch } from 'react-icons/fi'
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import { api } from "./services/api";
 
-import { Container } from './styles/styles'
+import { Container } from "./styles/styles";
 
+type AppProps = {
+  cep: string,
+  logradouro: string,
+  complemento: string,
+  bairro: string,
+  localidade: string,
+  uf: string
+}
 
 export function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState<AppProps>({} as AppProps);
+
+  async function handleSearch() {
+    if (input === "") {
+      alert("Preencha algum CEP");
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Erro ao buscar CEP");
+      setInput("");
+    }
+  } 
+
   return (
     <Container>
-      <h1 className='title'>Buscador CEP</h1>
+      <h1 className="title">Buscador CEP</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu CEP"/>
+        <input
+          type="text"
+          placeholder="Digite seu CEP"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF" />
         </button>
       </div>
 
-        <main className='main'>
-          <h2>CEP: 2298792</h2>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
 
-          <span>Rua Teste da Silva</span>
-          <span>Complemento: teste da silva</span>
-          <span>Vila Teste</span>
-          <span>Teste - TS</span>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
         </main>
-
+      )}
     </Container>
-  )
+  );
 }
